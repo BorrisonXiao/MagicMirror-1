e8669221a9282aaffd9e88811737a9b744cce029
BorrisonXiao <borrisonxiao@gmail.com>
2019-02-16 21:18:27 -0500

    bugged

--- js/bankinginfo.js
+++ js/bankinginfo.js
@@ -1,22 +1,23 @@
 /* Get all customers info */
-async function getAllCustomers() {
+function getAllCustomers() {
   var customerListReq = new XMLHttpRequest();
   customerListReq.open(
     "GET",
     "http://api.reimaginebanking.com/customers?key=b44189b54ec952b7a6dec907e7e77773"
   );
-  customerListReq.onload = async () => {
+  var customerList;
+  customerListReq.onload = () => {
     /* Parse the returned JSON object */
-    var customerList = JSON.parse(customerListReq.responseText);
-
-    /* TODO: How to use the fetched data? */
-    console.log(customerList[0]);
-    var displayContainer = document.createElement("div");
-    displayContainer.append(customerList[0]._id);
-    var customerContainer = document.getElementsByClassName(
-      "customerContainer"
-    )[0];
-    customerContainer.appendChild(displayContainer);
+    customerList = JSON.parse(customerListReq.responseText);
   };
+  /* TODO: How to use the fetched data? */
+  console.log(customerList[0]);
+  var displayContainer = document.createElement("div");
+  displayContainer.append(customerList[0]._id);
+  var customerContainer = document.getElementsByClassName(
+    "customerContainer"
+  )[0];
+  customerContainer.appendChild(displayContainer);
+
   customerListReq.send();
 }
