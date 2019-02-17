e1777bfc5bf40109ac4107c6656b9c4909a1e00b
BorrisonXiao <borrisonxiao@gmail.com>
2019-02-16 21:27:16 -0500

    Fixed the bug

--- js/bankinginfo.js
+++ js/bankinginfo.js
@@ -9,15 +9,18 @@ function getAllCustomers() {
   customerListReq.onload = () => {
     /* Parse the returned JSON object */
     customerList = JSON.parse(customerListReq.responseText);
+    console.log(customerList[0]);
+
+    /* Create a container in html to hold the parsed results */
+    var displayContainer = document.createElement("div");
+    displayContainer.append(customerList[0]._id);
+
+    /* Locate the element in the html file */
+    var customerContainer = document.getElementsByClassName(
+      "customerContainer"
+    )[0];
+    customerContainer.appendChild(displayContainer);
   };
-  /* TODO: How to use the fetched data? */
-  console.log(customerList[0]);
-  var displayContainer = document.createElement("div");
-  displayContainer.append(customerList[0]._id);
-  var customerContainer = document.getElementsByClassName(
-    "customerContainer"
-  )[0];
-  customerContainer.appendChild(displayContainer);
 
   customerListReq.send();
 }
