47d45df7cffbb62e59caf5a63be14debc144d1b6
BorrisonXiao <borrisonxiao@gmail.com>
2019-02-17 03:00:24 -0500

    Funnnnnnnnn voice control

--- js/voice.js
+++ js/voice.js
@@ -1,14 +1,22 @@
 $(document).ready(function() {
   navigator.mediaDevices.getUserMedia({
     audio: true
-  })
+  });
 });
 
 if (annyang) {
   // Let's define a command.
   var commands = {
-    'hello': function() {
-      alert('Hello world!');
+    hello: function() {
+      alert("Hello world!");
+    },
+    "close the map": function() {
+      var displayContainer = document.getElementsByClassName("mapContainer")[0];
+      displayContainer.hidden = true;
+    },
+    "show me the map": function() {
+      var displayContainer = document.getElementsByClassName("mapContainer")[0];
+      displayContainer.hidden = false;
     }
   };
 
@@ -17,4 +25,9 @@ if (annyang) {
 
   // Start listening.
   annyang.start();
+
+  annyang.addCallback("result", function(phrases) {
+    console.log("I think the user said: ", phrases[0]);
+    console.log("But then again, it could be any of the following: ", phrases);
+  });
 }
